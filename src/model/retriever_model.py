317e919e62850024d631e2a72fd064b061f2e558
Hung Nguyen <hungnq131002@gmail.com>
2023-11-01 13:08:16 +0700

    feat: use sentence transformer create embeddings with gpu/multiprocess

--- src/model/retriever_model.py
+++ src/model/retriever_model.py
@@ -39,14 +39,46 @@ def get_ctx_embd(
     """
     model_encoder.eval()
     encoded_input = tokenizer(
-            text,
-            padding=True,
-            truncation=True,
-            max_length=512,
-            return_tensors="pt"
-            ).to(device)
+        text,
+        padding=True,
+        truncation=True,
+        max_length=512,
+        return_tensors="pt",
+    ).to(device)
     with torch.no_grad():
         model_output = model_encoder(**encoded_input)
+        batch_embeds = model_output["pooler_output"].cpu().detach().numpy()
+        batch_embeds = [list(batch_embeds[i, :].reshape(-1).astype(float))
+                            for i in range(len(batch_embeds))
+                        ]
+    return batch_embeds
 
-    return model_output["pooler_output"]
+
+def get_ctx_emb_sbert( 
+        model_name_pr_path: str,
+        batch_contents : Union[str, List[str]],
+        target_devices: List[str] = None
+        ) -> torch.tensor:
+    """
+    Using multiple processes (1 per GPU), which encode sentences in parallel. 
+    This gives a near linear speed-up when encoding large text collections.
+    """
+    # handling batch_contents but only having one sentence
+    if isinstance(batch_contents, str):
+        batch_contents = [batch_contents]
+
+    from sentence_transformers import SentenceTransformer
+    # model = SentenceTransformer('all-MiniLM-L6-v2')
+    model = SentenceTransformer(model_name_pr_path)
+    # Start the multi-process pool on all available CUDA devices
+    pool = model.start_multi_process_pool(target_devices=target_devices)
+
+    # Compute the embeddings using the multi-process pool
+    batch_embeds = model.encode_multi_process(batch_contents, pool)
+    print("Embeddings computed. Shape:", batch_embeds.shape)
+
+    #O ptional: Stop the proccesses in the pool
+    model.stop_multi_process_pool(pool)
+
+    return batch_embeds
 

91e32d328c0b87eb0a43148d95b46d0bc8331bec
Hung Nguyen <hungnq131002@gmail.com>
2024-01-16 13:06:12 +0700

    chore: add code to get embedding size, add missing model_dim arguement when create table, add release and drop indexes functions to ensure data not in memory and only has one index type for each fields

--- src/run.py
+++ src/run.py
@@ -19,7 +19,7 @@ HOST=os.getenv("HOST", "localhost")
 PORT=os.getenv("PORT", "19530")
 TB_WIKI=os.getenv("TB_WIKI", "wiki_tb")
 TB_CLIENT=os.getenv("TB_CLIENT", "client_tb")
-BATCH=int(os.getenv("BATCH", 200))
+BATCH=int(os.getenv("BATCH", 10000))
 
 logging.getLogger().setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
@@ -28,27 +28,39 @@ def main():
     arguments = Arguments()
     args = arguments.parse()
     # row = 17553713 if args.dataset_version == "wiki40b_en_100_0" else 33849898
-    row = 1000 if args.dataset_version == "wiki40b_en_100_0" else 1000
+    row = 1000000 if args.dataset_version == "wiki40b_en_100_0" else 1000000
     # connect milvus server
     create_milvus_connection(host=HOST, port=PORT)
+
     # start to create
     if not args.just_create_index:
+        # Init collection
         collection = None
+
+        # Initialize model
+        # model = SentenceTransformer('all-MiniLM-L6-v2')
+        model = SentenceTransformer(args.model_name_or_path)
+        # specify target_devices for multi_gpus/gpu/cpu
+        target_devices = ['cuda:{}'.format(i) for i in range(torch.cuda.device_count())]
+        # get dim of output's embedding
+        sentences = ["only for get embeddings"]
+        embeddings = model.encode(sentences)
+        model_dim = int(embeddings.shape[-1])
+
+        # check if create collection
         if args.init_tb:
             if utility.has_collection(TB_WIKI):
                 print("Drop collection")
                 utility.drop_collection(TB_WIKI)
             print("Create collection")
-            collection = make_database.create_wiki_table()
+            # create collection with dim of model's embedding
+            collection = make_database.create_wiki_table(
+                model_dim=model_dim
+            )
         elif not utility.has_collection(TB_WIKI): 
             raise ValueError("Not having table wiki, please initialize first with `--init_tb` arguements")
         else: 
             collection = Collection(TB_WIKI)  
-
-        # model = SentenceTransformer('all-MiniLM-L6-v2')
-        model = SentenceTransformer(args.model_name_pr_path)
-        # specify target_devices for multi_gpus/gpu/cpu
-        target_devices = ['cuda:{}'.format(i) for i in range(torch.cuda.device_count())]
         
         print("Start downloading dataset")
         wiki_snippets = make_data.download_dataset(
@@ -66,10 +78,20 @@ def main():
             batch_insert=BATCH,
             limit_samples=row
         )
+        print("building index for tables")
         make_database.build_indexs(collection=collection)
     else:
-        logger.warning("Only index initialization is perfomed, make sure your table is filled up with data.")
-        make_database.build_indexs(collection=collection)
+        # logger.warning("Only index initialization is perfomed, make sure your table is filled up with data.")
+        print("Only index initialization is perfomed, make sure your table is filled up with data.")
+        collection = Collection(TB_WIKI)  
+        collection.release()
+        collection.drop_index(
+            filed_name="embedding",
+        )
+        make_database.build_indexs(
+            collection=collection,
+            filed_name="embedding"
+        )
 
 if __name__=="__main__":
     main()
